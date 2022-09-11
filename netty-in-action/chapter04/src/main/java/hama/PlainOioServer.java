94332a3b00cd56a18488ec7bad9c6b4ce943f673
kyungrae <rlarudfo93@gmail.com>
2022-09-12 00:01:06 +0900

    [netty] Transports

---
+++ netty-in-action/chapter04/src/main/java/hama/PlainOioServer.java
@@ -0,0 +1,40 @@
+package hama;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class PlainOioServer {
+    public void serve(int port) throws IOException {
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for (; ; ) {
+                final Socket clientSocket = socket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream out;
+                        try {
+                            out = clientSocket.getOutputStream();
+                            out.write("Hi!\n".getBytes(Charset.forName("UTF-8")));
+                            out.flush();
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException e) {
+                            }
+                        }
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
