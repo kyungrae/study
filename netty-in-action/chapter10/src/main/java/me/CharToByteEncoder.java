8ead5573f626822b49f1cff193454b361d670548
kyungrae <rlarudfo93@gmail.com>
2024-09-12 11:01:06 +0900

    [Netty] 10장 코덱 프레임워크

---
+++ netty-in-action/chapter10/src/main/java/me/CharToByteEncoder.java
@@ -0,0 +1,12 @@
+package me;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class CharToByteEncoder extends MessageToByteEncoder<Character> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Character msg, ByteBuf out) {
+        out.writeChar(msg);
+    }
+}
---
+++ netty-in-action/chapter10/src/main/java/me/IntegerToStringEncoder.java
@@ -0,0 +1,13 @@
+package me;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+public class IntegerToStringEncoder extends MessageToMessageEncoder<Integer> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Integer msg, List<Object> out) {
+        out.add(String.valueOf(msg));
+    }
+}
